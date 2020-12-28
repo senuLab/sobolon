c72253016b8f05fdb278a681aa555fce850e1869
senuLab <kondo.issei11@gmail.com>
2020-12-28 23:36:09 +0900

    scroll実装

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,16 @@ $(document).ready(function() {
   });
 
   new WOW().init();
+
+  $('.p-header__nav-link').click(function () {
+
+    let header = $(".l-header").outerHeight(); 
+    /* let speed = 300; */
+    let id = $(this).attr("href");
+    let position = $(id).offset().top - header;
+
+    $('body,html').animate({
+      scrollTop: position
+    }, 300);
+    return false;
+  });
